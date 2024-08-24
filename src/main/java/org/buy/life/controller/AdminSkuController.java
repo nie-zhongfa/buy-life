c8764067a99f1fc9e38f60335f6d5a3643be4eec
YourJustin <wuxiaobiao@bilibili.com>
2024-08-24 13:04:53 +0800

    admin

--- src/main/java/org/buy/life/controller/AdminSkuController.java
+++ src/main/java/org/buy/life/controller/AdminSkuController.java
@@ -1,10 +1,15 @@
 package org.buy.life.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @menu TODO
  * @Author YourJustin
  * @Date 2024/8/24 12:27 PM
  * I am a code man ^_^ !!
  */
+@RestController
+@RequestMapping("/admin/sku")
 public class AdminSkuController {
 }
