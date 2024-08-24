3c013611144c8fac692c014501bf40d662dcd95c
YourJustin <wuxiaobiao@bilibili.com>
2024-08-24 14:59:04 +0800

    admin

---
+++ src/main/java/org/buy/life/service/IAdminAccountService.java
@@ -0,0 +1,10 @@
+package org.buy.life.service;
+
+/**
+ * @menu TODO
+ * @Author YourJustin
+ * @Date 2024/8/24 2:55 PM
+ * I am a code man ^_^ !!
+ */
+public interface IAdminAccountService {
+}
---
+++ src/main/java/org/buy/life/service/impl/AdminOrderServiceImpl.java
@@ -0,0 +1,16 @@
+package org.buy.life.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.buy.life.service.IAdminOrderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @menu TODO
+ * @Author YourJustin
+ * @Date 2024/8/24 2:56 PM
+ * I am a code man ^_^ !!
+ */
+@Slf4j
+@Service
+public class AdminOrderServiceImpl implements IAdminOrderService {
+}
