b1bce1de09597189d225e5525ac5922d9329c795
niezhongfa <niezhongfa@bilibili.com>
2024-09-03 21:48:43 +0800

    1

--- src/main/java/org/buy/life/model/enums/ClassificationEnum.java
+++ src/main/java/org/buy/life/model/enums/ClassificationEnum.java
@@ -16,9 +16,9 @@ public enum ClassificationEnum {
     /**
      * 分类：genshin_impact、star_rail、zenless_zone_zero
      */
-    GENSHIN_IMPACT("genshin_impact", "原神"),
-    STAR_RAIL("star_rail", "崩坏星穹铁道"),
-    ZENLESS_ZONE_ZERO("zenless_zone_zero", "绝区零");
+    GENSHIN_IMPACT("genshin_impact", "{\"zh_cn\": \"原神\",\"en\": \"原神\",\"es\": \"原神\",\"fr\": \"原神\",\"de\": \"原神\"}"),
+    STAR_RAIL("star_rail", "{\"zh_cn\": \"崩坏星穹铁道\",\"en\": \"崩坏星穹铁道\",\"es\": \"崩坏星穹铁道\",\"fr\": \"崩坏星穹铁道\",\"de\": \"崩坏星穹铁道\"}"),
+    ZENLESS_ZONE_ZERO("zenless_zone_zero", "{\"zh_cn\": \"绝区零\",\"en\": \"绝区零\",\"es\": \"绝区零\",\"fr\": \"绝区零\",\"de\": \"绝区零\"}");
 
     private final String code;
     private final String desc;
