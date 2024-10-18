766071073d082b49beca6e00b566a3cc82bedf94
YourJustin <wuxiaobiao@bilibili.com>
2024-10-18 22:07:30 +0800

    1

--- src/main/java/org/buy/life/model/dto/ExportOrderDetailInfoDto.java
+++ src/main/java/org/buy/life/model/dto/ExportOrderDetailInfoDto.java
@@ -1,6 +1,9 @@
 package org.buy.life.model.dto;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentRowHeight;
+import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,71 +19,84 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@HeadRowHeight(30)
+@ContentRowHeight(20)
 public class ExportOrderDetailInfoDto {
 
     /**
      * 订单号
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "订单号",index = 0)
     private String orderId;
 
     /**
      * 用户id
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "用户名",index = 1)
     private String userId;
 
     /**
      * 邮箱
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "邮箱",index = 2)
     private String mail;
 
     /**
      * 商品id
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "商品编码",index = 3)
     private String skuId;
 
     /**
      * 商品名称
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "商品名称",index = 4)
     private String skuName;
 
     /**
      * 材质品类
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "材质品类",index = 5)
     private String skuCategory;
 
     /**
      * 人物款式
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "人物款式",index = 6)
     private String skuType;
 
     /**
      * 单价
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "单价",index = 7)
     private String price;
 
     /**
      * 金额
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "金额",index = 8)
     private String totalAmt;
 
     /**
      * 币种
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "币种",index = 9)
     private String currency;
 
     /**
      * 数量
      */
+    @ColumnWidth(30)
     @ExcelProperty(value = "数量",index = 10)
     private Long skuNum;
 }
--- src/main/java/org/buy/life/model/dto/ImportCategoryDto.java
+++ src/main/java/org/buy/life/model/dto/ImportCategoryDto.java
@@ -1,6 +1,9 @@
 package org.buy.life.model.dto;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentRowHeight;
+import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,17 +20,23 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@HeadRowHeight(30)
+@ContentRowHeight(20)
 public class ImportCategoryDto {
 
+    @ColumnWidth(30)
     @ExcelProperty(value = "品类编码",index = 0)
     private String categoryCode;
 
+    @ColumnWidth(30)
     @ExcelProperty(value = "IP",index = 1)
     private String ip;
 
+    @ColumnWidth(30)
     @ExcelProperty(value = "中文",index = 2)
     private String zh_cn;
 
+    @ColumnWidth(30)
     @ExcelProperty(value = "英文",index = 3)
     private String en;
 
