95d03355ad386f68d15dff4cb5eba5bd05e9d573
niezhongfa <niezhongfa@bilibili.com>
2024-11-06 11:21:58 +0800

    1

--- src/main/java/org/buy/life/filter/BuyLifeFilter.java
+++ src/main/java/org/buy/life/filter/BuyLifeFilter.java
@@ -76,7 +76,8 @@ public class BuyLifeFilter implements Filter {
         }
         if(request.getRequestURI().contains("/buyUser/doLogin") || request.getRequestURI().contains("/admin/login")||
                 request.getRequestURI().contains("/buyUser/create")||request.getRequestURI().contains("/buySkuDict/dict")
-                ||request.getRequestURI().contains("/buyUser/resendPwd")||request.getRequestURI().contains("/buySku/skuList")){
+                ||request.getRequestURI().contains("/buyUser/resendPwd")||request.getRequestURI().contains("/buySku/skuList")||
+                request.getRequestURI().contains("/buySeries/seriesList")||request.getRequestURI().contains("/buySku/skuBySeriesCode")){
             filterChain.doFilter(request, response);
         }else {
             try {
