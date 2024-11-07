fddf45b7e8ba46e92e2e4061ee46160b9d0aac05
niezhongfa <niezhongfa@bilibili.com>
2024-11-07 20:18:11 +0800

    1

--- src/main/java/org/buy/life/service/impl/BuyCategoryServiceImpl.java
+++ src/main/java/org/buy/life/service/impl/BuyCategoryServiceImpl.java
@@ -31,9 +31,9 @@ public class BuyCategoryServiceImpl extends ServiceImpl<BuyCategoryMapper, BuyCa
 
 
     @Override
-    public List<BuyCategoryEntity> getCategoryList(List<String> classifications){
-        if(CollectionUtils.isNotEmpty(classifications)){
-            return lambdaQuery().in(BuyCategoryEntity::getClassification,classifications).eq(BuyCategoryEntity::getIsDeleted, false).list();
+    public List<BuyCategoryEntity> getCategoryList(List<String> categoryLists){
+        if(CollectionUtils.isNotEmpty(categoryLists)){
+            return lambdaQuery().in(BuyCategoryEntity::getCategoryCode,categoryLists).eq(BuyCategoryEntity::getIsDeleted, false).list();
         }
         return lambdaQuery().eq(BuyCategoryEntity::getIsDeleted, false).list();
     }
